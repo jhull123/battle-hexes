a1b16073ee0c27b20f50a6393ac27f32c725cd4c
jhull123 <tarballer01@gmail.com>
2024-10-06 20:04:38 -0500

    extract domain classes to seperate files

--- battle-hexes-web/unit.js
+++ battle-hexes-web/unit.js
@@ -1,4 +1,19 @@
 class Unit {
+  #movePath = [];
+  #containingHex = undefined;
+
   constructor() {
+  }
+
+  getContainingHex() {
+    return this.#containingHex;
+  }
+
+  setContainingHex(aHex) {
+    this.#containingHex = aHex;
+  }
+
+  addToMovePath(hexToMoveTo) {
+    this.#movePath.add(hexToMoveTo);
   }
 }
