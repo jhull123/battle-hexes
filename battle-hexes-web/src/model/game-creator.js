134be7ff69781cf984765f0b8dce0c12d3080ac0
jhull123 <tarballer01@gmail.com>
2025-01-18 16:30:31 -0600

    game creator updates

--- battle-hexes-web/src/model/game-creator.js
+++ battle-hexes-web/src/model/game-creator.js
@@ -2,11 +2,13 @@ import { Board } from "./board";
 import { Faction } from "./faction";
 import { Game } from "./game";
 import { Player, Players } from "./player";
+import { Unit } from "./unit";
 
 export class GameCreator {
   createGame(gameData) {
     const board = new Board(gameData.board.rows, gameData.board.columns);
     const game = new Game(gameData.id, ['Movement', 'Combat'], this.#getPlayers(gameData), board);
+    this.#addUnits(board, game.getPlayers(), gameData.board);
     return game;
   }
 
@@ -25,5 +27,24 @@ export class GameCreator {
       factions.push(new Faction(faction.name, faction.counterColor));
     }
     return factions;
+  }
+
+  #addUnits(board, players, boardData) {
+    const factionMap = this.#getFactionMap(players);
+    for (let unitData of boardData.units) {
+      const faction = factionMap.get(unitData.faction.name);
+      const unit = new Unit(unitData.name, faction);
+      board.addUnit(unit, unitData.row, unitData.column);
+    }
+  }
+
+  #getFactionMap(players) {
+    const factionMap = new Map();
+    for (let player of players.getAllPlayers()) {
+      for (let faction of player.getFactions()) {
+        factionMap.set(faction.getName(), faction);
+      }
+    }
+    return factionMap;
   }
 }
--- battle-hexes-web/tests/model/game-creator.test.js
+++ battle-hexes-web/tests/model/game-creator.test.js
@@ -1,11 +1,15 @@
 import { GameCreator } from "../../src/model/game-creator";
+import { Hex } from "../../src/model/hex";
 
 let game;
 
 beforeEach(() => {
   const gameCreator = new GameCreator();
   const gameData = JSON.parse(
-    '{"id":"093e432e-28ba-4dd1-a202-0802ee6ef32b","players":[{"name":"Player 1","type":"Human","factions":[{"name":"Red Faction","color":"#C81010"}]},{"name":"Player 2","type":"Computer","factions":[{"name":"Blue Faction","color":"#4682B4"}]}],"board":{"rows":10,"columns":10,"units":[{"row":6,"column":4,"faction":{"name":"Red Faction","color":"#C81010"}},{"row":3,"column":5,"faction":{"name":"Blue Faction","color":"#4682B4"}}]}}'
+    '{"id":"093e432e-28ba-4dd1-a202-0802ee6ef32b","players":[{"name":"Player 1","type":"Human","factions":[{"name":"Red Faction","color":"#C81010"}]},{"name":"Player 2","type":"Computer","factions":[{"name":"Blue Faction","color":"#4682B4"}]}],' +
+    '"board":{"rows":10,"columns":10,"units":[' +
+    '{"name":"Red Unit","row":6,"column":4,"faction":{"name":"Red Faction","color":"#C81010"}},' +
+    '{"name":"Blue Unit","row":3,"column":5,"faction":{"name":"Blue Faction","color":"#4682B4"}}]}}'
   );
   game = gameCreator.createGame(gameData);
 });
@@ -34,13 +38,20 @@ describe("createGame", () => {
     expect(game.getPhases()[0]).toBe("Movement");
     expect(game.getPhases()[1]).toBe("Combat");
   });
-/*
+
   test("board size is set", () => {
-    expect(game.getBoard().getHex(10, 10)).toBeInstanceOf(Hex); 
+    expect(game.getBoard().getHex(9, 9)).toBeInstanceOf(Hex); 
   });
 
   test("board has two units", () => {
-    expect(game.getBoard().getUnits()).toBe(2)
+    expect(game.getBoard().getUnits().size).toBe(2)
   });
-  */
+
+  test("board has units in correct hexes", () => {
+    const unit1 = game.getBoard().getHex(6, 4).getUnits().values().next().value;
+    const unit2 = game.getBoard().getHex(3, 5).getUnits().values().next().value;
+
+    expect(unit1.getName()).toBe("Red Unit");
+    expect(unit2.getName()).toBe("Blue Unit");
+  });
 });
