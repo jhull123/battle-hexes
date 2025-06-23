6efc1801fcc07ed334b415b6471e9d74860db61d
jhull123 <tarballer01@gmail.com>
2025-06-23 18:21:15 -0500

    random player impl

--- battle-hexes-api/src/cpu/randomplayer.py
+++ battle-hexes-api/src/cpu/randomplayer.py
@@ -3,6 +3,7 @@ from typing import List, Set
 from src.game.board import Board
 from src.game.hex import Hex
 from src.game.player import Player
+from src.game.unitmovementplan import UnitMovementPlan
 
 
 class RandomPlayer:
@@ -12,7 +13,8 @@ class RandomPlayer:
         self.player = player
         self.board = board
 
-    def movement(self) -> List | None:
+    def movement(self) -> List[UnitMovementPlan]:
+        plans = []
         for unit in self.player.own_units(self.board.get_units()):
             starting_hex = self.board.get_hex(unit.row, unit.column)
             reachable_hexes = self.board.get_reachable_hexes(
@@ -24,7 +26,14 @@ class RandomPlayer:
                 f"Unit {unit.name} is moving from {starting_hex} to "
                 f"{selected_hex}"
             )
-        return None
+            path = self.board.shortest_path(
+                unit,
+                starting_hex,
+                selected_hex
+            )
+            if path:
+                plans.append(UnitMovementPlan(unit, path))
+        return plans
 
     def random_hex(self, hexes: Set[Hex]) -> Hex:
         """Select a random hex from the given set of hexes."""
--- battle-hexes-api/tests/cpu/test_randomplayer.py
+++ battle-hexes-api/tests/cpu/test_randomplayer.py
@@ -16,38 +16,32 @@ class TestRandomPlayer(unittest.TestCase):
 
         self.board = Board(10, 10)
         self.blue_faction = Faction(id=uuid.uuid4(), name='Blue', color='blue')
-        self.blue_player = Player(
+        blue_player = Player(
             name='Blue Player',
             type=PlayerType.CPU,
             factions=[self.blue_faction],
         )
         self.blue_player_rando = RandomPlayer(
-            player=self.blue_player,
+            player=blue_player,
             board=self.board
         )
-        self.game = Game(players=[self.blue_player], board=self.board)
+        self.game = Game(players=[blue_player], board=self.board)
         self.blue1 = Unit(
             uuid.uuid4(),
             'Blue Unit 1',
             self.blue_faction,
-            self.blue_player,
+            blue_player,
             'Infantry',
             2, 1, 3
         )
-        self.board.add_unit(self.blue1, 5, 5)
 
     def tearDown(self):
         # Restore the original random state
         random.setstate(self._random_state)
 
-    def test_empty_test(self):
+    def test_no_moves_for_empty_board(self):
         moves = self.blue_player_rando.movement()
-        print("Moves:", moves)
-        # TODO more test logic here !
-
-    # def test_no_moves_for_empty_board(self):
-    #     moves = self.blue_player.movement(self.game)
-    #     self.assertEqual(moves, [], "Expected no moves for an empty board")
+        self.assertEqual(moves, [], "Expected no moves for an empty board")
 
     # def test_move_one_unit(self):
     #     self.board.add_unit(self.blue1, 5, 5)
