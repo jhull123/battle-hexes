49066f7d36435bc11eb1bf88b8b1fdd429d2ad92
jhull123 <tarballer01@gmail.com>
2025-12-01 07:06:15 -0600

    add placeholder village drawer

---
+++ battle-hexes-web/src/drawer/village-drawer.js
@@ -0,0 +1,16 @@
+export class VillageDrawer {
+  #p;
+  #hexDrawer;
+
+  constructor(p, hexDrawer) {
+    this.#p = p;
+    this.#hexDrawer = hexDrawer;
+  }
+
+  draw(aHex) {
+    // TODO implement me!
+    console.log(this.#p);
+    console.log(this.#hexDrawer);
+    console.log(aHex);
+  }
+}
