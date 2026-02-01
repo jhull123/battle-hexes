79a8f22280c64ad0b4e2e03bc6898c0d7039594c
jhull123 <tarballer01@gmail.com>
2026-02-01 10:25:24 -0600

    more village art

--- battle-hexes-web/src/terraindraw/village-drawer.js
+++ battle-hexes-web/src/terraindraw/village-drawer.js
@@ -20,7 +20,32 @@ export class VillageDrawer {
     let fillIndex = 0;
 
     const blocks = [
-      { dx: -0.55, dy: -0.5, w: 1.15, h: 0.78, angle: -0.28 }
+      // outbuildings
+      { dx: -0.42, dy: -0.72, w: 1.15, h: 0.78, angle: -0.20 },
+      { dx: -0.19, dy: -0.75, w: 0.55, h: 0.62, angle: -0.21 },
+
+      { dx: 0.51, dy: 0.68, w: 0.81, h: 0.75, angle: -0.03 },
+      { dx: 0.27, dy: 0.66, w: 0.75, h: 0.76, angle:  0.01 },
+
+      // first row
+      { dx: -0.39, dy: -0.19, w: 1.20, h: 0.79, angle:  0.04 },
+      { dx: -0.40, dy:  0.19, w: 1.22, h: 0.82, angle: -0.07 },
+
+      { dx: -0.05, dy: -0.16, w: 1.20, h: 0.79, angle: -0.02 },
+      { dx: -0.06, dy:  0.16, w: 1.22, h: 0.82, angle:  0.02 },
+      
+      { dx: 0.24, dy: -0.16, w: 1.20, h: 0.79, angle: -0.06 },
+      { dx: 0.26, dy:  0.16, w: 1.22, h: 0.82, angle:  0.02 },
+      
+      // second row top
+      { dx: -0.23, dy: -0.45, w: 1.06, h: 0.74, angle: 0.04 },
+      { dx:  0.16, dy: -0.40, w: 1.30, h: 0.74, angle: -0.04 },
+
+      // second row bottom
+      { dx: -0.16, dy:  0.44, w: 1.36, h: 0.78, angle: -0.08 },
+
+      // church
+      { dx: 0.59, dy:  0.01, w: 1.25, h: 1.84, angle:  0.02 }
     ];
 
     blocks.forEach(({ dx, dy, w, h, angle }) => {
